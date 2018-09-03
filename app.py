9a2a64900ed7c31d7edfef183a11ffadd35b5920
Lucas Chapin <lucas@altschool.com>
2018-09-03 14:09:24 -0400

    moved server config to init file

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
-import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from flask import Flask, redirect, url_for
-from flask_dance.contrib.google import make_google_blueprint, google
 
-from auth.google_oauth import GoogleOAuth
-
-server = Flask(__name__)
-
-app = dash.Dash(__name__, server=server, url_base_pathname='/', auth='auth')
-authorized_emails = ["lucas.chapin@gmail.com", "1234@example.com"]
-auth = GoogleOAuth(app, authorized_emails)
+from app import app, server
 
 @server.route("/")
 def MyDashApp():
